9666bc153ad58876b01f5dd27f8eac98357b4516
Bochra-Mayara <bochramayara122@gmail.com>
2024-08-27 14:28:50 +0200

    Test

--- src/test/java/com/bochra/UnitTest/service/PersonServiceTest.java
+++ src/test/java/com/bochra/UnitTest/service/PersonServiceTest.java
@@ -2,7 +2,7 @@ package com.bochra.UnitTest.service;
 
 import com.bochra.UnitTest.exception.InvalidPersonException;
 import com.bochra.UnitTest.model.Person;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,10 +13,44 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PersonServiceTest {
+
+
     @Autowired
     private PersonService personService;
 
 
+    /*before class method */
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Inside the before all methods");
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Inside the after all methods");
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Inside the before each method");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Inside the after each method");
+    }
+
+    @Test
+    public void testMethod1(){
+        System.out.println("My first test method");
+    }
+
+    @Test
+    public void testMethod2(){
+        System.out.println("My second test method");
+    }
     @Test
     public void shouldSavaPerson() {
         Person personToSave = new Person("John Doe", 30);
@@ -35,9 +69,9 @@ public class PersonServiceTest {
 
 
         // Act & Assert
-        assertThrows(InvalidPersonException.class, () -> {
-            new Person(null, 30);
-        }, "Name cannot be null or empty");
+        var exp = assertThrows(InvalidPersonException.class, () ->
+            new Person(null, 30));
+        assertEquals("Name cannot be null or empty", exp.getMessage());
 
 
     }
